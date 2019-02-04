62be044ff0c9af9f3d41cf52afa334796d8ff1dd
mistermur <brian.j.murillo@gmail.com>
2019-02-04 17:13:13 -0500

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,26 @@
+class Song < ActiveRecord::Base
+  validates :title, presence: true
+  validates :title, uniqueness: {
+    scope: %i[release_year artist_name],
+    message: 'cannot be repeated by the same artist in the same year'
+  }
+  validates :released, inclusion: { in: [true, false] }
+  validates :artist_name, presence: true
+
+  with_options if: :released? do |song|
+    song.validates :release_year, presence: true
+    song.validates :release_year, numericality: {
+      less_than_or_equal_to: Date.today.year
+    }
+  end
+
+  def released?
+    released
+  end
+end
+
+  # def
+  #   def validate_each(record, attribute, value)
+  #     record.errors[attribute] << (options[:message] || 'must be blank') unless record.send(attribute).blank?
+  #   end
+  # end
