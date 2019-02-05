f477d6360f2b2724a29bc0bebc0538289366bf13
Linya Hu <lh62594@yahoo.com>
2019-02-05 09:22:13 -0500

    Done.

---
+++ db/seeds.rb
@@ -0,0 +1,2 @@
+Song.create(title: "song 1", released: "true", release_year: "2010", artist_name: "t-swift", genre: "pop")
+Song.create(title: "song 2", released: "true", release_year: "2018", artist_name: "katy perry", genre: "pop")
